98a159f87f387941a7d876005a143979c83c9705
Prasad Tammana <prasadgithub@gmail.com>
2017-02-08 16:30:07 -0800

    Replaced some custom code with sinon spy/stub features.

--- test/unit/sender-test.js
+++ test/unit/sender-test.js
@@ -9,63 +9,67 @@ const anyPort = 1234;
 const anyIpv4 = '1.2.3.4';
 const anyIpv6 = '2002:20:0:0:0:0:1:3';
 const anyRequest = new Buffer(0x02);
+
 const udpIpv4 = 'udp4';
 const udpIpv6 = 'udp6';
 
-class SocketStub extends EventEmitter {
-  constructor(sendSuccess) {
+const sendResultSuccess = 0;
+const sendResultError = 1;
+const sendResultCancel = 2;
+
+class FakeSocket extends EventEmitter {
+  constructor(result) {
     super();
-    this.sendSuccess = sendSuccess;
-    this.validationData = {};
+    this.result = result;
   }
 
   send(buffer, offset, length, port, ipAddress) {
-    this.validationData.buffer = buffer;
-    this.validationData.offset = offset;
-    this.validationData.port = port;
-    this.validationData.ipAddress = ipAddress;
-
     process.nextTick(this.responseHandler.bind(this));
   }
 
   responseHandler() {
-    if (this.sendSuccess) {
-      this.emit('message', this);
-    } else {
+    if (this.result === sendResultError) {
       this.emit('error', this);
+    } else {
+      this.emit('message', this);
     }
   }
 
   close() {
-    this.validationData.closed = true;
   }
 }
 
-const sendToIpAddressImpl = function(test, sinon, ipAddress, udpVersionExpected, sendSuccess) {
-  sinon.stub(Dgram, 'createSocket').withArgs(udpVersionExpected).returns(new SocketStub(sendSuccess));
+const sendToIpAddressImpl = function(test, sinon, ipAddress, udpVersionExpected, result) {
+  const fakeSocket = new FakeSocket(result);
+  const socketSendSpy = sinon.spy(fakeSocket, 'send');
+  const socketCloseSpy = sinon.spy(fakeSocket, 'close');
+  sinon.stub(Dgram, 'createSocket').withArgs(udpVersionExpected).returns(fakeSocket);
 
   const multiSubnetFailover = false;
   const sender = new Sender(ipAddress, anyPort, anyRequest, multiSubnetFailover);
+
   sender.execute((error, message) => {
-    let socketStub;
-    if (sendSuccess) {
+    if (result === sendResultSuccess) {
       test.strictEqual(error, null);
-      test.ok(message instanceof SocketStub);
-      socketStub = message;
-    } else {
+      test.strictEqual(message, fakeSocket);
+    } else if (result === sendResultError) {
+      test.strictEqual(error, fakeSocket);
       test.strictEqual(message, undefined);
-      test.ok(error instanceof SocketStub);
-      socketStub = error;
+    } else {
+      test.ok(false, 'Should never get here.', error, message);
     }
 
-    test.strictEqual(socketStub.validationData.buffer, anyRequest);
-    test.strictEqual(socketStub.validationData.offset, 0);
-    test.strictEqual(socketStub.validationData.port, anyPort);
-    test.strictEqual(socketStub.validationData.ipAddress, ipAddress);
-    test.strictEqual(socketStub.validationData.closed, true);
-
+    test.ok(socketCloseSpy.withArgs().calledOnce);
     test.done();
   });
+
+  test.ok(socketSendSpy.withArgs(anyRequest, 0, anyRequest.length, anyPort, ipAddress).calledOnce);
+
+  if (result === sendResultCancel) {
+    sender.cancel();
+    test.ok(socketCloseSpy.withArgs().calledOnce);
+    test.done();
+  }
 };
 
 exports['Sender send to IP address'] = {
@@ -80,27 +84,18 @@ exports['Sender send to IP address'] = {
   },
 
   'send to IPv4': function(test) {
-    sendToIpAddressImpl(test, this.sinon, anyIpv4, udpIpv4, true);
+    sendToIpAddressImpl(test, this.sinon, anyIpv4, udpIpv4, sendResultSuccess);
   },
 
   'send to IPv6': function(test) {
-    sendToIpAddressImpl(test, this.sinon, anyIpv6, udpIpv6, true);
+    sendToIpAddressImpl(test, this.sinon, anyIpv6, udpIpv6, sendResultSuccess);
   },
 
   'send fails': function(test) {
-    sendToIpAddressImpl(test, this.sinon, anyIpv4, udpIpv4, false);
+    sendToIpAddressImpl(test, this.sinon, anyIpv4, udpIpv4, sendResultError);
   },
 
   'send cancel': function(test) {
-    this.sinon.stub(Dgram, 'createSocket').withArgs(udpIpv4).returns(new SocketStub(true));
-    const multiSubnetFailover = false;
-    const sender = new Sender(anyIpv4, anyPort, anyRequest, multiSubnetFailover);
-    sender.execute((error, message) => {
-      test.ok(false, 'Should never get here.', error, message);
-    });
-
-    sender.cancel();
-
-    test.done();
+    sendToIpAddressImpl(test, this.sinon, anyIpv4, udpIpv4, sendResultCancel);
   }
 };
