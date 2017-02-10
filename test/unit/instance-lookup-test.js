aa85cdc0ebf48215a13dc704f8294f8b46922d94
Prasad Tammana <prasadgithub@gmail.com>
2017-02-10 15:16:57 -0800

    Argument validation tests for instanceLookup.

--- test/unit/instance-lookup-test.js
+++ test/unit/instance-lookup-test.js
@@ -1,6 +1,64 @@
 'use strict';
 
 const parse = require('../../src/instance-lookup').parseBrowserResponse;
+const instanceLookup = require('../../src/instance-lookup').instanceLookup;
+
+exports['instanceLookup invalid args'] = {
+  'invalid server': function(test) {
+    const expectedErrorMessage = 'Invalid arguments: "server" must be a string';
+    try {
+      const notString = 4;
+      instanceLookup({ server: notString });
+    } catch (err) {
+      test.strictEqual(err.message, expectedErrorMessage);
+      test.done();
+    }
+  },
+
+  'invalid instanceName': function(test) {
+    const expectedErrorMessage = 'Invalid arguments: "instanceName" must be a string';
+    try {
+      const notString = 4;
+      instanceLookup({ server: 'serverName', instanceName: notString });
+    } catch (err) {
+      test.strictEqual(err.message, expectedErrorMessage);
+      test.done();
+    }
+  },
+
+  'invalid timeout': function(test) {
+    const expectedErrorMessage = 'Invalid arguments: "timeout" must be a number';
+    try {
+      const notNumber = 'some string';
+      instanceLookup({ server: 'server', instanceName: 'instance', timeout: notNumber });
+    } catch (err) {
+      test.strictEqual(err.message, expectedErrorMessage);
+      test.done();
+    }
+  },
+
+  'invalid retries': function(test) {
+    const expectedErrorMessage = 'Invalid arguments: "retries" must be a number';
+    try {
+      const notNumber = 'some string';
+      instanceLookup({ server: 'server', instanceName: 'instance', timeout: 1000, retries: notNumber });
+    } catch (err) {
+      test.strictEqual(err.message, expectedErrorMessage);
+      test.done();
+    }
+  },
+
+  'invalid callback': function(test) {
+    const expectedErrorMessage = 'Invalid arguments: "callback" must be a function';
+    try {
+      const notFunction = 4;
+      instanceLookup({ server: 'server', instanceName: 'instance', timeout: 1000, retries: 3 }, notFunction);
+    } catch (err) {
+      test.strictEqual(err.message, expectedErrorMessage);
+      test.done();
+    }
+  }
+};
 
 exports.oneInstanceFound = function(test) {
   const response = 'ServerName;WINDOWS2;InstanceName;SQLEXPRESS;IsClustered;No;Version;10.50.2500.0;tcp;1433;;';
